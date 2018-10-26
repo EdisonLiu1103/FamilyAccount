b9c0644347e5a0c4259124b6148f6f4912c39f74
Edison <edisonliu311@gmail.com>
2018-10-26 15:31:02 +0800

    完成设置收入时间

--- app/src/main/java/com/example/edison/familyacoount/AddInaccount.java
+++ app/src/main/java/com/example/edison/familyacoount/AddInaccount.java
@@ -1,13 +1,78 @@
 package com.example.edison.familyacoount;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.Calendar;
 
 public class AddInaccount extends AppCompatActivity {
+
+
+    protected static final int DATE_DIALOG_ID = 0;          //日期对话框常量
+    EditText txtInMoney, txtInTime, txtInHandler, txtInMark;
+    Spinner spInType;
+    Button btnInSaveButton;
+    Button btnInCancelButton;
+    private int mYear;
+    private int mMonth;
+    private int mDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_inaccount);
+
+        txtInMoney = findViewById(R.id.txtInMoney);
+        txtInTime = findViewById(R.id.txtInTime);
+        txtInHandler = findViewById(R.id.txtInHandler);
+        txtInHandler = findViewById(R.id.txtInHandler);
+        spInType = findViewById(R.id.spInType);
+        btnInSaveButton = findViewById(R.id.btnInSave);
+        btnInCancelButton = findViewById(R.id.btnInCancel);
+
+        txtInTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(DATE_DIALOG_ID);
+            }
+        });
+
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        updateDisplay();
     }
+
+    private void updateDisplay(){
+        txtInTime.setText(new StringBuilder().append(mYear).append("-").append(mMonth + 1).append("-").append(mDay));
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id){
+        switch (id){
+            case DATE_DIALOG_ID:
+                return new DatePickerDialog(this, mDateListener, mYear, mMonth, mDay);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener mDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+            mYear = i;
+            mMonth = i1;
+            mDay = i2;
+            updateDisplay();
+        }
+    };
 }
+
